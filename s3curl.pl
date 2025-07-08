62e902e074e7df026d2872d741599323018e907c
root <shilpa.rangappa@dell.com>
2025-07-08 18:32:39 +0000

    Add s3curl changes

--- s3curl.pl
+++ s3curl.pl
@@ -216,13 +216,20 @@ for (my $i=0; $i<@ARGV; $i++) {
             $resource = "/";
         }
         my @attributes = ();
+		my $query_param = 'query';
+        my $attributes_param='attributes';
         for my $attribute ("acl", "delete", "location", "logging", "notification",
             "partNumber", "policy", "requestPayment", "response-cache-control",
             "response-content-disposition", "response-content-encoding", "response-content-language",
             "response-content-type", "response-expires", "torrent",
             "uploadId", "uploads", "versionId", "versioning", "versions", "website", "lifecycle", "restore", "query", "searchmetadata", "fanout",
-			"fileaccess", "searchmetadata", "query", "cors", "isstaleallowed") {
+			"fileaccess", "searchmetadata", "query", "cors", "isstaleallowed","attributes") {
             if ($query =~ /(?:^|&)($attribute)=?([^&]+)?(?:&|$)/) {
+				if (grep($query_param,@attributes)){
+					if($attribute==$attributes_param){
+						next;
+					}
+				}
                 my $kv_pair = sprintf("%s%s", $1, $2 ? sprintf("=%s", $2) : '');
 
                 push @attributes, uri_unescape($kv_pair);
